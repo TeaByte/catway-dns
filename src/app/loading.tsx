4bd227f1fe67a9d25ca658ee21c7d7eb2c91b794
Yazan <100817706+TeaByte@users.noreply.github.com>
2024-05-01 21:42:50 +0300

    Update loading spinner width/hight

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -4,7 +4,7 @@ export default function Loading() {
   return (
     <main className="mx-4 mt-6 flex flex-col items-center justify-center gap-6 md:mx-[200px] md:mt-10 lg:mx-[300px] xl:mx-[400px] 2xl:mx-[700px]">
       <div className="flex flex-col items-center gap-2">
-        <LoaderCircle className="h-28 w-28 animate-spin" />
+        <LoaderCircle className="h-24 w-24 animate-spin" />
         <p className="text-center text-lg font-semibold">Loading content</p>
       </div>
     </main>
